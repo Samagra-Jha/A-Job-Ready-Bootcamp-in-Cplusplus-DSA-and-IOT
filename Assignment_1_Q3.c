4f1b6522705dfa4792526b895ecd9c9c40b2a677
Samagra-Jha <114701694+Samagra-Jha@users.noreply.github.com>
2022-09-30 05:58:27 +0530

    Add files via upload

---
+++ Assignment_1_Q3.c
@@ -0,0 +1,10 @@
+//Input and output in C Language
+//3. Write a program to print “MySirG” on the screen. (Remember to print in double quotes)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\"MySirG\"");
+    getch();
+    return 0;
+}
