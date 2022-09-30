07452b216bdc73c3bb5aacdb27030ca0f105e097
Samagra-Jha <114701694+Samagra-Jha@users.noreply.github.com>
2022-09-30 06:45:31 +0530

    Add files via upload

---
+++ Assignment_1_Q5.c
@@ -0,0 +1,9 @@
+//5. WAP to calculate the length of String using printf function.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n%d",printf("ABC"));
+    getch();
+    return 0;
+}
